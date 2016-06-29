215321f34527399eb86fc569a6592a80349792df
14rcole <rcyoalne@gmail.com>
2016-06-29 16:18:49 -0400

    add basic Commit() function

--- pkg/otbuiltin/commit.go
+++ pkg/otbuiltin/commit.go
@@ -4,12 +4,53 @@ import (
   glib "github.com/14rcole/ostree-go/pkg/glibobject"
 )
 
-func Commit(options map[string]string) {
+// #cgo pkg-config: ostree-1
+// #include <stdlib.h>
+// #include <glib.h>
+// #include <ostree.h>
+// #include "builtin.go.h"
+import "C"
 
-}
+const nilOptions CommitOptions = nil
+var options CommitOptions = nilOptions
 
-func parseArgs(options map[string]string) {
+func Commit(path string, opts CommitOptions) error {
+  // Parse the arguments
+  if opts != nilOptions {
+    options = opts
+  }
 
+  // Prepare for the Commit
+  repo, err := openRepo(path)
+  if err != nil {
+    return err
+  }
+  // Start the transaction
+  cerr := (*C.GError)(gerr.Ptr())
+  prepared := glib.GoBool(glib.GBoolean(C.ostree_repo_prepare_transaction(repo, C.FALSE, nil, &cerr)))
+  if !prepared {
+    return glib.ConvertGError(glib.GBoolean(unsafe.Pointer(cerr)))
+  }
+  // Create an #OstreeMutableTree
+  var mutableTree *C.OstreeMutableTree = nil
+  C.ostree_mutable_tree_init(mutableTree)
+  // Write metadata
+  cerr = nil
+  cpath := C.CString(path)
+  written := glib.GoBool(glib.GBoolean(ostree_repo_write_mtree(repo, &mutableTree,GFile **out_file C.g_file_new_for_path(cpath), nil, &cerr)))
+  if !written {
+    return glib.ConvertGError(glib.GBoolean(unsafe.Pointer(cerr)))
+  }
+  // Create a commit
+  cerr = nil
+  csubject := C.CString(options.Subject)
+  cbody := C.CString(options.Body)
+  var output *C.char = nil
+  committed := glib.GoBool(glib.GBoolean(ostree_repo_write_commit(repo, nil, csubject, cbody, nil, mutableTree, output, C.g_cancellable_new(), &cerr)))
+  if !committed {
+    return glib.ConvertGError(glib.GBoolean(unsafe.Pointer(cerr)))
+  }
+  return nil
 }
 
 
