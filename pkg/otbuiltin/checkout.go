7c9952881a6f373438ef6dadf96d91e194d367cd
14rcole <rcyoalne@gmail.com>
2016-06-30 16:22:49 -0400

    process one checkout with DisableCache set

--- pkg/otbuiltin/checkout.go
+++ pkg/otbuiltin/checkout.go
@@ -17,6 +17,12 @@ import (
 import "C"
 
 var options CheckoutOptions
+type options struct {
+  userOpts          CheckoutOptions
+  mode              int
+  overwriteMode     int
+}
+var opts options
 
 type CheckoutOptions struct {
   UserMode        bool    // Do not change file ownership or initialize extended attributes
@@ -27,15 +33,36 @@ type CheckoutOptions struct {
   FromFile        string  // Process many checkouts from the given file
 }
 
-func Checkout(repo, filepath, commit string, opts CheckoutOptions) error {
-  if opts != (CheckoutOptions{}) {
-    options = opts
+func Checkout(repo, filepath, commit string, options CheckoutOptions) error {
+  if options != (CheckoutOptions{}) {
+    opts.userOpts = options
   }
   return nil
 }
 
 func processOneCheckout(OstreeRepo *repo, resolved_commit, subpath, destination string, cancellable glib.GCancellable) error {
+  if options.DisableCache {
+    C.OstreeRepoCheckoutOptions options = nil;
 
+    if options.UserMode {
+      opts.mode = C.OSTREE_REPO_CHECKOUT_MODE_USER
+    }
+    if options.Union {
+      opts.overwriteMode = C.OSTREE_REPO_CHECKOUT_OVERWRITE_UNION_FILES
+    }
+
+    cdest := C.CString(destination)
+    ccommit := C.CString(resolved_commit)
+    var gerr = glib.NewGError()
+    cerr := (*C.GError)(gerr.Ptr())
+    checkedOut := glib.GoBool(glib.GBoolean(C.ostree_repo_checkout_tree_at(repo, options, C.AT_FWCWD, cdest, ccommit, nil, cerr)))
+    if !checkedOut {
+      return glib.ConvertGError(glib.ToGError(unsafe.Pointer(cerr)))
+    }
+    return nil
+  } else {
+    
+  }
 }
 
 func processManyCheckouts(OstreeRepo *repo, target string, cancellable glib.GCancellable) error {
