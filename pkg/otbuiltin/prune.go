a477d4c9090b90efe8c18d3ee45606ef898737ed
14rcole <rcyoalne@gmail.com>
2016-07-01 19:30:37 -0400

    add PruneOptions struct

--- pkg/otbuiltin/prune.go
+++ pkg/otbuiltin/prune.go
@@ -1,1 +1,24 @@
 package otbuiltin
+
+import (
+       "unsafe"
+       "time"
+
+       glib "github.com/14rcole/ostree-go/pkg/glibobject"
+)
+
+// #cgo pkg-config: ostree-1
+// #include <stdlib.h>
+// #include <glib.h>
+// #include <ostree.h>
+// #include "builtin.go.h"
+import "C"
+
+type PruneOptions struct {
+  NoPrune           bool      // Only display unreachable objects; don't delete
+  RefsOnly          bool      // Only compute reachability via refs
+  DeleteCommit      string    // Specify a commit to delete
+  KeepYoungerThan   time.Time // All commits older than this date will be pruned
+  Depth             int = -1  // Only traverse depths (integer) parents for each commit (default: -1=infinite)
+  StaticDeltasOnly  int       // Change the behavior of --keep-younger-than and --delete-commit to prune only the static delta files
+}
