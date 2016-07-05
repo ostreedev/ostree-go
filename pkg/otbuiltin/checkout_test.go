2514bed3c6c8fe53df7c26c6bc76aa131e72ccb1
14rcole <rcyoalne@gmail.com>
2016-07-05 10:09:22 -0400

    create checkout_test skeleton

---
+++ pkg/otbuiltin/checkout_test.go
@@ -0,0 +1,21 @@
+package otbuiltin
+
+import (
+       "testing"
+)
+
+func TestCheckoutSuccessProcessOne(t *testing.T) {
+
+}
+
+func TestCheckoutFailProcessOne(t *testing.T) {
+
+}
+
+func TestCheckoutSuccessProcessMany(t *testing.T) {
+
+}
+
+func TestCheckoutFailProcessMany(8 *testing.T) {
+
+}
