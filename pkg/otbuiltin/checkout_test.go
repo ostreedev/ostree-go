7907036b52d006467bf9f517a7857ab7c1c8377b
14rcole <rcyoalne@gmail.com>
2016-07-13 15:27:10 -0400

    modify checkout test

--- pkg/otbuiltin/checkout_test.go
+++ pkg/otbuiltin/checkout_test.go
@@ -2,20 +2,20 @@ package otbuiltin
 
 import (
        "testing"
-       _ "os"
-       _ "fmt"
-       _ "time"
+        "os"
+        "fmt"
+        "time"
 )
 
 func TestCheckoutSuccessProcessOneBranch(t *testing.T) {
   // Make a directory in which the repo should exist
-  /*testDir := "/tmp/test-init-repo"
-  err := os.Mkdir(testDir, 0777)
+  repoDir := "/tmp/test-init-repo"
+  err := os.Mkdir(repoDir, 0777)
   if err != nil {
     t.Errorf("%s", err)
     return
   }
-  defer os.RemoveAll(testDir)
+  defer os.RemoveAll(repoDir)
 
   // Initialize the repo
   inited, err := Init("/tmp/test-init-repo", nil)
@@ -27,30 +27,28 @@ func TestCheckoutSuccessProcessOneBranch(t *testing.T) {
   //Commit to the repo
   commitOpts := NewCommitOptions()
   branch := "test-branch"
-  ret, err := Commit(testDir, ".", branch, commitOpts)
+  ret, err := Commit(repoDir, "/home/rycole/Development/C-C++/ostree", branch, commitOpts)
   if err != nil {
     t.Errorf("%s", err)
   } else {
     fmt.Println(ret)
   }
 
-  fmt.Println("Sleeping so you can check the repo manually...")
-  d, _ := time.ParseDuration("30s")
-  time.Sleep(d)
-
   checkoutOpts := NewCheckoutOptions()
   checkoutDir := "/tmp/checkout"
-  err := os.Mkdir(checkoutDir, 0777)
+  err = os.Mkdir(checkoutDir, 0777)
   if err != nil {
     t.Errorf("%s", err)
     return
   }
-  defer os.RemoveAll(checkoutDir)*/
+  defer os.RemoveAll(checkoutDir)
 
-  //err = Checkout(testDir, checkoutDir, branch, checkoutOpts)
-  branch := "ot-branch"
+  fmt.Println("This is your opportunity to do a quick checkout yourself")
+  d, _ := time.ParseDuration("30s")
+  time.Sleep(d)
+
+  err = Checkout(repoDir, checkoutDir, branch, checkoutOpts)
   defer os.RemoveAll(branch)
-  err := Checkout("/tmp/repo", "/tmp/ot-branch", "ot-branch", checkoutOpts)
   if err != nil {
     t.Errorf("%s", err)
     return
