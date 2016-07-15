800a734b11402cd083da7cf3dc6b431878947af9
14rcole <rcyoalne@gmail.com>
2016-07-15 14:32:52 -0400

    fix init test bug that was causing commit and prune to fail

--- pkg/otbuiltin/init_test.go
+++ pkg/otbuiltin/init_test.go
@@ -5,34 +5,30 @@ import (
 	"testing"
 )
 
-func TestInit(t *testing.T) {
-	// Create an empty directory is we know it's not a repo
-	testDir := "/tmp/test-init-repo"
-	err := os.Mkdir(testDir, 0777)
+func TestInitSuccess(t *testing.T) {
+	// Make a base directory in which all of our test data resides
+	baseDir := "/tmp/otbuiltin-test/"
+	err := os.Mkdir(baseDir, 0777)
 	if err != nil {
 		t.Errorf("%s", err)
 		return
 	}
-	defer os.RemoveAll(testDir)
-
-	// Try to init the repo
-	// In this case, inited should be true and err should be nil
-	inited, err := Init("/tmp/test-init-repo", nil)
-	if !inited || err != nil {
+	defer os.RemoveAll(baseDir)
+	// Make a directory in which the repo should exist
+	repoDir := baseDir + "repo"
+	err = os.Mkdir(repoDir, 0777)
+	if err != nil {
 		t.Errorf("%s", err)
 		return
 	}
 
-	// Try to init the repo
-	// In this case, inited should be true and err should be false
-	inited, err = Init("/tmp/test-init-repo", nil)
-	if !inited {
-		if err == nil {
-			t.Errorf("Error initing repo that already exists")
-			return
-		} else {
-			t.Errorf("%s", err)
-			return
-		}
+	// Initialize the repo
+	inited, err := Init(repoDir, nil)
+	if err != nil {
+		t.Errorf("%s", err)
+		return
+	} else if !inited {
+		t.Errorf("Cannot test commit: failed to initialize repo")
+		return
 	}
 }
