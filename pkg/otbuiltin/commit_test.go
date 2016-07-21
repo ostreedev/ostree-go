e5a2d4948cae5fdad424b9a535b3127a2da26324
14rcole <rcyoalne@gmail.com>
2016-07-21 13:30:49 -0400

    add test for tree instead of commitDir

--- pkg/otbuiltin/commit_test.go
+++ pkg/otbuiltin/commit_test.go
@@ -5,6 +5,8 @@ import (
 	"os"
 	"testing"
 
+  "github.com/jhoonb/archivex"
+
 	"github.com/14rcole/gopopulate"
 )
 
@@ -54,4 +56,61 @@ func TestCommitSuccess(t *testing.T) {
 	} else {
 		fmt.Println(ret)
 	}
+}
+
+func TestCommitTreeSuccess(t *testing.T) {
+	// Make a base directory in which all of our test data resides
+	baseDir := "/tmp/otbuiltin-test/"
+	err := os.Mkdir(baseDir, 0777)
+	if err != nil {
+		t.Errorf("%s", err)
+		return
+	}
+	defer os.RemoveAll(baseDir)
+	// Make a directory in which the repo should exist
+	repoDir := baseDir + "repo"
+	err = os.Mkdir(repoDir, 0777)
+	if err != nil {
+		t.Errorf("%s", err)
+		return
+	}
+
+	// Initialize the repo
+	inited, err := Init(repoDir, NewInitOptions())
+	if !inited || err != nil {
+		fmt.Println("Cannot test commit: failed to initialize repo")
+		return
+	}
+
+	//Make a new directory full of random data to commit
+	commitDir := baseDir + "commit1"
+	err = os.Mkdir(commitDir, 0777)
+	if err != nil {
+		t.Errorf("%s", err)
+		return
+	}
+	err = gopopulate.PopulateDir(commitDir, "rd", 4, 4)
+	if err != nil {
+		t.Errorf("%s", err)
+		return
+	}
+  tarPath := baseDir + "tree.tar"
+  tar := new(archivex.TarFile)
+  tar.Create(tarPath)
+  tar.AddAll(commitDir, true)
+  tar.Close()
+
+
+	//Test commit
+	opts := NewCommitOptions()
+  opts.Subject = "blob"
+  opts.Tree = []string{"tar=" + tarPath}
+	branch := "test-branch"
+	ret, err := Commit(repoDir, "", branch, opts)
+	if err != nil {
+		t.Errorf("%s", err)
+	} else {
+		fmt.Println(ret)
+	}
+
 }
