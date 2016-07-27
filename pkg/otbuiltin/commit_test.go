f50e5c52ddf3389d0262ed83deef317324af856b
14rcole <rcyoalne@gmail.com>
2016-07-27 15:44:49 -0400

    add commit test for parent checksum

--- pkg/otbuiltin/commit_test.go
+++ pkg/otbuiltin/commit_test.go
@@ -5,8 +5,6 @@ import (
 	"os"
 	"testing"
 
-  "github.com/jhoonb/archivex"
-
 	"github.com/14rcole/gopopulate"
 )
 
@@ -94,20 +92,79 @@ func TestCommitTreeSuccess(t *testing.T) {
 		t.Errorf("%s", err)
 		return
 	}
-  tarPath := baseDir + "tree.tar"
-  tar := new(archivex.TarFile)
-  tar.Create(tarPath)
-  tar.AddAll(commitDir, true)
-  tar.Close()
-
+	tarPath := baseDir + "tree.tar"
+	gopopulate.Tar(commitDir, tarPath)
 
 	//Test commit
 	opts := NewCommitOptions()
-  opts.Subject = "blob"
-  opts.Tree = []string{"tar=/tmp/pkg.tar"}
-  opts.TarAutoCreateParents = true
+	opts.Subject = "blob"
+	opts.Tree = []string{"tar=" + tarPath}
+	opts.TarAutoCreateParents = true
 	branch := "test-branch"
 	ret, err := Commit(repoDir, "", branch, opts)
+	if err != nil {
+		t.Errorf("%s", err)
+	} else {
+		fmt.Println(ret)
+	}
+
+}
+
+func TestCommitTreeParentSuccess(t *testing.T) {
+	// Make a base directory in which all of our test data resides
+	baseDir := "/tmp/otbuiltin-test/"
+	err := os.Mkdir(baseDir, 0777)
+	if err != nil {
+		t.Errorf("%s", err)
+		return
+	}
+	defer os.RemoveAll(baseDir)
+	// Make a directory in which the repo should exist
+	repoDir := baseDir + "repo"
+	err = os.Mkdir(repoDir, 0777)
+	if err != nil {
+		t.Errorf("%s", err)
+		return
+	}
+
+	// Initialize the repo
+	inited, err := Init(repoDir, NewInitOptions())
+	if !inited || err != nil {
+		fmt.Println("Cannot test commit: failed to initialize repo")
+		return
+	}
+
+	//Make a new directory full of random data to commit
+	commitDir := baseDir + "commit1"
+	err = os.Mkdir(commitDir, 0777)
+	if err != nil {
+		t.Errorf("%s", err)
+		return
+	}
+	err = gopopulate.PopulateDir(commitDir, "rd", 4, 4)
+	if err != nil {
+		t.Errorf("%s", err)
+		return
+	}
+	tarPath := baseDir + "tree.tar"
+	gopopulate.Tar(commitDir, tarPath)
+
+	//Test commit
+	opts := NewCommitOptions()
+	opts.Subject = "blob"
+	opts.Tree = []string{"tar=" + tarPath}
+	opts.TarAutoCreateParents = true
+	branch := "test-branch"
+	ret, err := Commit(repoDir, "", branch, opts)
+	if err != nil {
+		t.Errorf("%s", err)
+	} else {
+		fmt.Println(ret)
+	}
+
+	// Commit again, this time with a parent checksum
+	opts.Parent = ret
+	ret, err = Commit(repoDir, "", branch, opts)
 	if err != nil {
 		t.Errorf("%s", err)
 	} else {
