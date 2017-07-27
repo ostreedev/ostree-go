93e63e1ee65134cc95696e904e66457b0db009df
Sjoerd Simons <sjoerd.simons@collabora.co.uk>
2017-07-27 12:21:51 +0200

    Add wrapper for ostree_repo_remote_add
    Signed-off-by: Sjoerd Simons <sjoerd.simons@collabora.co.uk>

--- pkg/otbuiltin/remote.go
+++ pkg/otbuiltin/remote.go
@@ -1,1 +1,33 @@
 package otbuiltin
+
+import (
+	glib "github.com/ostreedev/ostree-go/pkg/glibobject"
+	"unsafe"
+)
+
+// #cgo pkg-config: ostree-1
+// #include <stdlib.h>
+// #include <glib.h>
+// #include <ostree.h>
+// #include "builtin.go.h"
+import "C"
+
+func (repo *Repo) RemoteAdd(name, url string, options *glib.GVariant,
+	cancellable *glib.GCancellable) error {
+
+	var cerr *C.GError = nil
+
+	cname := C.CString(name)
+	defer C.free(unsafe.Pointer(cname))
+
+	curl := C.CString(url)
+	defer C.free(unsafe.Pointer(curl))
+
+	r := C.ostree_repo_remote_add(repo.native(), cname, curl, (*C.GVariant)(options.Ptr()), cCancellable(cancellable), &cerr)
+
+	if !gobool(r) {
+		return generateError(cerr)
+	}
+
+	return nil
+}
